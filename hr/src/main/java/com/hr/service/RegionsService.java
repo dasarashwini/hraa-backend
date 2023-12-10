1b361acce95ae4378076fdea76c26e0bee30706c
yogamahiv <yogamahi.venkatesan@capgemini.com>
2023-12-10 22:24:00 +0530

    Region Service Completed

--- hr/src/main/java/com/hr/service/RegionsService.java
+++ hr/src/main/java/com/hr/service/RegionsService.java
@@ -5,13 +5,14 @@ import java.util.List;
 import org.springframework.stereotype.Service;
 
 import com.hr.dto.RegionsPojo;
+import com.hr.exceptions.ResourceAlreadyExistsException;
 
 
 @Service
 public interface RegionsService {
 	
-	String addRegion(RegionsPojo newRegion);
-	String updateRegion(RegionsPojo newRegion);
+	String addRegion(RegionsPojo newRegion) throws ResourceAlreadyExistsException;
+	String updateRegion(RegionsPojo newRegion) throws ResourceAlreadyExistsException;
 	List<RegionsPojo> getAllRegions();
 	RegionsPojo getRegionById(int regionId);
 	String deleteRegionById(int regionId);	
